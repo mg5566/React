0ab0ad591982567ad7857993a122a8583b1c92db
강명구 <mg5566@hanmail.net>
2021-12-30 15:42:05 +0900

    refactor: remove hard cording data to dynamic data

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date();
+  const expenseTitle = "Desktop PC";
+  const expenseAmount = "200.00"
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
