8dc2daa86b780abc5f0f5940c233a3b897bbe14c
강명구 <mg5566@hanmail.net>
2022-01-03 20:01:16 +0900

    feat: add state and vaild logic

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -1,20 +1,53 @@
+import { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card.js";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log("addUserHandler");
+    console.log("addUserHandler", enteredUserName, enteredAge);
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setIsValid(false);
+      return ;
+    }
+    if (+enteredAge < 1) {
+      setIsValid(false);
+      return ;
+    }
+    setEnteredUserName("");
+    setEnteredAge('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUserName}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         {/* <button type="submit">Add User</button> */}
         <Button type="submit">Add User</Button>
       </form>
