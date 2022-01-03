73d288bf9948db170761c5753a3f240bf0448d7a
강명구 <mg5566@hanmail.net>
2022-01-03 19:40:27 +0900

    feat: create Card and Button component

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
-import "./AddUser.css";
+import styles from "./AddUser.module.css";
 import Card from "../UI/Card.js";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -8,13 +9,14 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className="adduser">
+    <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        {/* <button type="submit">Add User</button> */}
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
