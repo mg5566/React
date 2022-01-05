4f63bf6ade965c156f7fdb0e45b817ab3ea039bf
강명구 <mg5566@hanmail.net>
2022-01-05 19:36:10 +0900

    feat: create new project

---
+++ food-order-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+        제발 취업 시켜주세요...
+        취업이 하고 싶니?
+        그럼 'build Food Order App~'
+        Okay Let's move
+    </div>
+  );
+}
+
+export default App;
