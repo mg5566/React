7f765f1526fdca7b2fe5c5a2070b5fe5bf1bc354
강명구 <mg5566@hanmail.net>
2022-01-06 00:53:22 +0900

    feat: create Meal item form component

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,5 @@
+import MealItemForm from "./MealItemForm";
+
 import styles from "./MealItem.module.css";
 
 const MealItem = (props) => {
@@ -9,6 +11,9 @@ const MealItem = (props) => {
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
