5f38c960b0733292709c2c3c6f9b0809b5721bc8
강명구 <mg5566@hanmail.net>
2022-01-10 15:42:49 +0900

    feat: add cart state reducer

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,18 +1,51 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCarthandler = (id) => {};
+const cartRuducer = (state, action) => {
+  if (action.type === "ADD_CART_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.prie * action.item.amount;
+    return ({
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    });
+  } else if (action.type === 'REMOVE_CART_ITEM') {
+
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispathCartAction] = useReducer(
+    cartRuducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispathCartAction({ type: "ADD_CART_ITEM", item: item });
+  };
+
+  const removeItemFromCarthandler = (id) => {
+    dispathCartAction({ type: "REMOVE_CART_ITEM", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCarthandler,
   };
 
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
