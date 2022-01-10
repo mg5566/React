8bfa13a9efc2d8100143705469c361cfc6a66691
강명구 <mg5566@hanmail.net>
2022-01-10 21:21:02 +0900

    feat: add logic for removing cart item

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -8,9 +8,12 @@ const defaultCartState = {
 
 const cartRuducer = (state, action) => {
   if (action.type === "ADD_CART_ITEM") {
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
 
-    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
@@ -25,12 +28,29 @@ const cartRuducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
-    return ({
+    return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
-    });
-  } else if (action.type === 'REMOVE_CART_ITEM') {
+    };
+  } else if (action.type === "REMOVE_CART_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
 
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
