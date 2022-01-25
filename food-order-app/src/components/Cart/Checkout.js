5eb8310331ae6247c147083f040fd1da8fa6c70b
강명구 <mg5566@hanmail.net>
2022-01-25 22:20:48 +0900

    feat: add logic of reading form values

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -1,32 +1,56 @@
+import { useRef } from "react";
 import styles from "./Checkout.module.css";
 
+const isEmpty = (value)=>value.trim() !== "";
+const isNotFiveChars = (value)=>value.trim().length !== 5;
+
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
-    console.log('Checkout', 'confirmHandler');
-  }
+
+    const eteredName = nameInputRef.current.value;
+    const eteredStreet = streetInputRef.current.value;
+    const eteredPostalCode = postalCodeInputRef.current.value;
+    const eteredCity = cityInputRef.current.value;
+
+    console.log(
+      "Checkout",
+      "confirmHandler",
+      eteredName,
+      eteredStreet,
+      eteredPostalCode,
+      eteredCity
+    );
+  };
 
   return (
     <form className={styles.form} onSubmit={confirmHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="postalCode">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalCodeInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <div className={styles.actions}>
-      <button type="button" onClick={props.onCancle}>Cancle</button>
-      <button className={styles.submit}>Confirm</button>
+        <button type="button" onClick={props.onCancle}>
+          Cancle
+        </button>
+        <button className={styles.submit}>Confirm</button>
       </div>
     </form>
   );
