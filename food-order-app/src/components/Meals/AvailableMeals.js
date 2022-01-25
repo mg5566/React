5584139ab07bfc7712432fe36e8d78aeadf5ad14
강명구 <mg5566@hanmail.net>
2022-01-25 16:54:22 +0900

    feat: add error handlering

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -6,14 +6,18 @@ import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const [isLoading, setIsLoading] = useState(true);
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(); // undefined
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-26863-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -31,13 +35,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={styles.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.mealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
