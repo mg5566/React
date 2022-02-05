a0d614e68805e56a33ce0f260823ff806a9a6ba0
강명구 <mg5566@hanmail.net>
2022-02-06 00:21:28 +0900

    feat: apply auth slice

--- practice-redux/practice-redux/src/App.js
+++ practice-redux/practice-redux/src/App.js
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.authentication.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth ? <Auth /> : <UserProfile />}
       <Counter />
     </Fragment>
   );
