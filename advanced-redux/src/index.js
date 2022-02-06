66e90df7f5e0d1a7cd4c9c31d2640906106e972a
강명구 <mg5566@hanmail.net>
2022-02-06 20:38:29 +0900

    refactor: recopy source codes

--- advanced-redux/src/index.js
+++ advanced-redux/src/index.js
@@ -1,14 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
