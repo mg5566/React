fc0682405f9939a290a9575dda5353b84e349d57
강명구 <mg5566@hanmail.net>
2022-02-11 16:49:58 +0900

    feat: apply fetching cart data when app component called

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -4,7 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-actions";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -13,6 +13,10 @@ function App() {
   const isShowCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   // cart 가 최신화되면 backend 에 PUT request 를 보냅니다.
   useEffect(() => {
