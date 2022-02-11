2538e5579e4e96c4072a7c17752be93a64b83921
강명구 <mg5566@hanmail.net>
2022-02-11 16:12:27 +0900

    refactor: create cart actions

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -4,8 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
