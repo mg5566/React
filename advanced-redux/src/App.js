077f8946bce614a4eb96785d07ff6d6a129c5990
mg5566 <mg5566@hanmail.net>
2022-02-11 17:48:05 +0900

    19-advanced-redux (#8)
    * feat: add redux store
    
    * refactor: recopy source codes
    
    * feat: add new func
    
    * feat: add notification component and fetch process
    
    * feat: add thunk action creator
    
    * refactor: create cart actions
    
    * feat: add replacing fetch data
    
    * feat: add fetching cart data action
    
    * feat: apply fetching cart data when app component called
    
    * chore: fix bugs

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,9 +1,49 @@
-import Counter from './components/Counter';
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const isShowCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  // cart 가 최신화되면 backend 에 PUT request 를 보냅니다.
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
   return (
-    <Counter />
+    <Fragment>
+      {notification && (
+        <Notification
+          title={notification.title}
+          status={notification.status}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {isShowCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
