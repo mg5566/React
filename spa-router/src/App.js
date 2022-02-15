18c37fc50a5482c2bfb30facb957701b29fb8da0
강명구 <mg5566@hanmail.net>
2022-02-15 16:23:00 +0900

    feat: add navbar using Link of react router

--- spa-router/src/App.js
+++ spa-router/src/App.js
@@ -1,21 +1,26 @@
+import MainHeader from "./components/MainHeader";
 import { Route, Routes } from "react-router-dom";
 
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-      </Routes>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
+
+const Layout = () => {
+  return <h2>Let's get started!</h2>;
+};
 
 export default App;
