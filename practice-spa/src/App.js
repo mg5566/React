d2ab56a56348aa7fe50c6d8d9ae4c42042b0a23d
강명구 <mg5566@hanmail.net>
2022-02-18 16:56:10 +0900

    feat: add layout component

--- practice-spa/src/App.js
+++ practice-spa/src/App.js
@@ -1,18 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Routes>
         <Route path="/" element={<Navigate replace to="quotes" />} />
         <Route path="quotes" element={<AllQuotes />} />
         <Route path="quotes/:quoteId/*" element={<QuoteDetail />} />
         <Route path="new-quote" element={<NewQuote />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
