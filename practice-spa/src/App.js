ce272525b1ac4629e419224848bf54a464dc039d
강명구 <mg5566@hanmail.net>
2022-02-18 18:07:37 +0900

    feat: add not found page

--- practice-spa/src/App.js
+++ practice-spa/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="quotes" element={<AllQuotes />} />
         <Route path="quotes/:quoteId/*" element={<QuoteDetail />} />
         <Route path="new-quote" element={<NewQuote />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
