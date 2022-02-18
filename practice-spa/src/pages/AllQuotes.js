f470fe662d3a6ee1ad6fd60fff6f66aae4998bb3
강명구 <mg5566@hanmail.net>
2022-02-18 17:17:13 +0900

    feat: add dummy datas

--- practice-spa/src/pages/AllQuotes.js
+++ practice-spa/src/pages/AllQuotes.js
@@ -1,5 +1,18 @@
+import QuoteList from "../components/quotes/QuoteList.js";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kang", text: "the beginning is half" },
+  {
+    id: "q2",
+    author: "Kang",
+    text: "Strong against the strong, infinitely weak against the weak",
+  },
+  { id: "q3", author: "GAYLE", text: "A, B, C, D, E, Fuck U" },
+  { id: "q4", author: "Pythagoras", text: "There is no answer" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
