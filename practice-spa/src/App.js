15c4e94aa0ea6b0f5a70a4032423ba54493c4c91
강명구 <mg5566@hanmail.net>
2022-02-18 15:29:21 +0900

    feat: add pages and apply router

--- practice-spa/src/App.js
+++ practice-spa/src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="quotes" element={<AllQuotes />} />
+        <Route path="quotes/:quotesId" element={<QuoteDetail />} />
+        <Route path="new-quote" element={<NewQuote />} />
+      </Routes>
     </div>
   );
 }
+
+const MainPage = () => {
+  return <h1>Main page</h1>;
+};
 
 export default App;
