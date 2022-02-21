2cd9cd4be72df1015fa8410d40a8380ea6978c12
강명구 <mg5566@hanmail.net>
2022-02-21 13:20:12 +0900

    feat: add route navigation

--- practice-spa/src/pages/NewQuote.js
+++ practice-spa/src/pages/NewQuote.js
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm.js";
 
 const NewQuote = () => {
+  const navigate = useNavigate();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    // send req to server
+
+    // route to all quotes page
+    navigate("/");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
