47ee8f5636bd8f22dc1e23d48c1cc21a2e791430
mg5566 <mg5566@hanmail.net>
2022-02-22 23:01:48 +0900

    21-deploy-react-app (#11)
    * feat: add lazy loading for optimize:
    
    * feat: deploy apps

--- practice-spa/src/App.js
+++ practice-spa/src/App.js
@@ -1,20 +1,37 @@
+import React, { Suspense } from "react";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+// import NewQuote from "./pages/NewQuote";
+// import NotFound from "./pages/NotFound";
+// import QuoteDetail from "./pages/QuoteDetail";
+
+// add lazy loading using dynamic import
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="quotes" />} />
-        <Route path="quotes" element={<AllQuotes />} />
-        <Route path="quotes/:quoteId/*" element={<QuoteDetail />} />
-        <Route path="new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {/* add fallback component using Suspense component */}
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="quotes" />} />
+          <Route path="quotes" element={<AllQuotes />} />
+          <Route path="quotes/:quoteId/*" element={<QuoteDetail />} />
+          <Route path="new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
