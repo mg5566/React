8e816a8a3c8d2cbb342664deb267f90c5d508355
강명구 <mg5566@hanmail.net>
2022-02-24 00:55:29 +0900

    feat: add logout feature

--- auth-app/src/components/Layout/MainNavigation.js
+++ auth-app/src/components/Layout/MainNavigation.js
@@ -1,13 +1,21 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const navigate = useNavigate();
+
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    // redirection
+    navigate("/", { replace: true });
+  };
 
   return (
     <header className={classes.header}>
@@ -28,7 +36,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
