2f97bc70d03c8a95278f8cc5af83fa7bb09cf950
강명구 <mg5566@hanmail.net>
2022-02-24 14:07:50 +0900

    feat: add protect unauthorized page access

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,17 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import AuthPage from "./pages/AuthPage";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<div>NotFound</div>} />
+        <Route
+          path="/profile"
+          element={
+            authCtx.isLoggedIn ? (
+              <ProfilePage />
+            ) : (
+              <Navigate replace to="/auth" />
+            )
+          }
+        />
+        {/* <Route path="*" element={<div>NotFound</div>} /> */}
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </Layout>
   );
