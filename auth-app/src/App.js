72120dbb98da0eb48b92519d76f01d07eb8fe4c5
mg5566 <mg5566@hanmail.net>
2022-02-24 16:17:18 +0900

    22-authentication (#12)
    * feat: create authentication react app
    
    * feat: create Layout component
    
    * feat: create Main Page
    
    * feat: create Profile page
    
    * feat: create auth page
    
    * feat: add firebase REST api
    
    * refactor: modify logic to simple
    
    * feat: create store of authentication
    
    * feat: apply auth context
    
    * feat: add password change feature
    
    * feat: add page redirection
    
    * feat: add logout feature
    
    * feat: add protect unauthorized page access
    
    * feat: add auto logout feature

---
+++ auth-app/src/App.js
@@ -0,0 +1,34 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import AuthPage from "./pages/AuthPage";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
+
+function App() {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {!authCtx.isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
+        <Route
+          path="/profile"
+          element={
+            authCtx.isLoggedIn ? (
+              <ProfilePage />
+            ) : (
+              <Navigate replace to="/auth" />
+            )
+          }
+        />
+        {/* <Route path="*" element={<div>NotFound</div>} /> */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
