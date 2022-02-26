4e9be6d8f5c1c0229d2d1cfa7f6f8da194e2f18c
강명구 <mg5566@hanmail.net>
2022-02-26 15:06:24 +0900

    feat: add new meetup form page

--- meets-up/pages/new-meetup/index.js
+++ meets-up/pages/new-meetup/index.js
@@ -1,3 +1,15 @@
-const NewMeetup = () => {};
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetup = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log("NewMeetup", enteredMeetupData);
+
+    // send to backend server using REST api
+  };
+
+  return (
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  );
+};
 
 export default NewMeetup;
