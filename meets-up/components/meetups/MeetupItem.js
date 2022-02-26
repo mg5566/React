08021440d20897d24a305eb2c1592bad6325ad04
강명구 <mg5566@hanmail.net>
2022-02-26 15:30:42 +0900

    feat: apply global layout

--- meets-up/components/meetups/MeetupItem.js
+++ meets-up/components/meetups/MeetupItem.js
@@ -7,9 +7,9 @@ const MeetupItem = (props) => {
   return (
     <li className={classes.item}>
       <Card>
-        <div className={classes.image}>
+        {/* <div className={classes.image}>
           <Image src={props.image} alt={props.title} width={500} height={500} />
-        </div>
+        </div> */}
         <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
--- meets-up/pages/index.js
+++ meets-up/pages/index.js
@@ -1,25 +1,25 @@
-import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-  {id: 'm1',
-  title: "명지대",
-  address: "용인시 처인구",
-  image: "https://img.hankyung.com/photo/202202/AA.21218839.1.jpg",
-  description: "",
+  {
+    id: "m1",
+    title: "명지대",
+    address: "용인시 처인구",
+    image: "https://img.hankyung.com/photo/202202/AA.21218839.1.jpg",
+    description: "",
   },
-  {id: "m2",
-  title: "The Coffee Bean",
-  address: "서울시 강남구 역삼동",
-  image: "https://image.ajunews.com/content/image/2018/04/01/20180401151236565154.jpg",
-  description: "",
+  {
+    id: "m2",
+    title: "The Coffee Bean",
+    address: "서울시 강남구 역삼동",
+    image:
+      "https://image.ajunews.com/content/image/2018/04/01/20180401151236565154.jpg",
+    description: "",
   },
 ];
 
 const HomePage = () => {
-  return <Layout>
-    <MeetupList meets={DUMMY_MEETUPS} />
-  </Layout>
+  return <MeetupList meets={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
