3d68c06ae4a5d24b26f22fda8cf175435abceb10
강명구 <mg5566@hanmail.net>
2022-02-28 18:17:49 +0900

    feat: add new meetup apiusing mongoDB

---
+++ meets-up/pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+// our-domain.com/api/new-meetup
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    // const { image, title, description, address } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://test:test@fuckingdb.rufm6.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+};
+
+export default handler;
