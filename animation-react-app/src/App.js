8c8176cd7b574b37e1f07812b4c2199187b9df4c
강명구 <mg5566@hanmail.net>
2022-03-02 15:16:54 +0900

    feat: add Transition component

--- animation-react-app/src/App.js
+++ animation-react-app/src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 // import List from "./components/List/List";
 
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 const App = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [showBlock, setShowBlock] = useState(false);
 
   const showModal = () => {
     setModalIsOpen(true);
@@ -19,6 +28,27 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Animations</h1>
+      <button
+        className="Button"
+        onClick={() => setShowBlock((prevState) => !prevState)}
+      >
+        toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={600} mountOnEnter unmountOnExit>
+        {(state) => (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+              transition: "opacity 1s ease-out",
+              ...transitionStyles[state],
+            }}
+          ></div>
+        )}
+      </Transition>
       {modalIsOpen ? <Modal show={modalIsOpen} closed={closeModal} /> : null}
       {modalIsOpen ? <Backdrop show={modalIsOpen} /> : null}
       <button className="Button" onClick={showModal}>
