7b3b61d4698f68992b364b89f873b8912060be4e
강명구 <mg5566@hanmail.net>
2022-03-02 22:39:21 +0900

    feat: add Product page

--- replacing-redux/src/App.js
+++ replacing-redux/src/App.js
@@ -2,16 +2,18 @@ import { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Navigation from "./components/Nav/Navigation"
+import Favorites from './containers/Favorites';
+import Products from './containers/Products';
 
 function App() {
   return (
     <Fragment>
       <Navigation />
-      {/* <Routes>
-        <Route path="/" element={} />
-        <Route path="/favorites" element={} />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/favorites" element={<Favorites />} />
         <Route path="/*" element={<p>404 Not Found</p>} />
-      </Routes> */}
+      </Routes>
     </Fragment>
   );
 }
