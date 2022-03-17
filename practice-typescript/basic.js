5bd68bff3702a789c73df5e8bf44c9b5f96e3a00
강명구 <mg5566@hanmail.net>
2022-03-17 15:17:51 +0900

    feat: add basic types

---
+++ practice-typescript/basic.js
@@ -0,0 +1,11 @@
+// primitive types
+// 1. number
+var age;
+age = 30;
+// 2. string
+var userName;
+userName = "Mike";
+// 3. boolean
+var isStudent;
+isStudent = false;
+// null 과 undefined 는 나중에
---
+++ practice-typescript/basic.ts
@@ -0,0 +1,14 @@
+// primitive types
+// 1. number
+let age: number;
+age = 30;
+
+// 2. string
+let userName: string;
+userName = "Mike";
+
+// 3. boolean
+let isStudent: boolean;
+isStudent = false;
+
+// null 과 undefined 는 나중에
