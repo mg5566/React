ef44ef45936b00cad70dd7b7ea3ded226fceacf4
강명구 <mg5566@hanmail.net>
2022-03-18 01:31:28 +0900

    feat: add state and apply types

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [
-    new Todo("Learn React"),
-    new Todo("Learn TypeScript"),
-    new Todo("Learn Next.js"),
-  ];
+  // const todos = [
+  //   new Todo("Learn React"),
+  //   new Todo("Learn TypeScript"),
+  //   new Todo("Learn Next.js"),
+  // ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+
+    setTodos((prev) => {
+      return prev.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
