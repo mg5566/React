f4614cde74722ff2d334727e7697c6fda234239e
강명구 <mg5566@hanmail.net>
2022-03-18 22:25:07 +0900

    feat: add useState hook

--- project-hooks/src/components/Ingredients/IngredientForm.js
+++ project-hooks/src/components/Ingredients/IngredientForm.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 
 import "./IngredientForm.css";
 
 const IngredientForm = () => {
+  const inputState = useState({ title: "", amount: "" });
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
@@ -13,11 +16,31 @@ const IngredientForm = () => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input id="title" type="text" />
+            <input
+              id="title"
+              type="text"
+              value={inputState[0].title}
+              onChange={(event) => {
+                inputState[1]((prevState) => ({
+                  title: event.target.value,
+                  amount: prevState.amount,
+                }));
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input id="amount" type="text" />
+            <input
+              id="amount"
+              type="number"
+              value={inputState[0].amount}
+              onChange={(event) => {
+                inputState[1]((prevState) => ({
+                  amount: event.target.value,
+                  title: prevState.title,
+                }));
+              }}
+            />
           </div>
           <div className="ingredient-form__action">
             <button type="submit">Add Ingredient</button>
