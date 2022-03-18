94234f35c01b8d4c06b1f338fe5d22ac56335ebd
강명구 <mg5566@hanmail.net>
2022-03-19 01:29:17 +0900

    feat: load filtered ingredients

--- project-hooks/src/components/Ingredients/Search.js
+++ project-hooks/src/components/Ingredients/Search.js
@@ -1,14 +1,47 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 
 import "./Search.css";
 
-const Search = () => {
+const Search = (props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  useEffect(() => {
+    console.log("Search render", enteredFilter);
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch(
+      "https://react-26863-default-rtdb.firebaseio.com/ingredient.json" + query
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const newIngredientsArray = [];
+        for (const key in responseData) {
+          newIngredientsArray.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        // set data
+        onLoadIngredients(newIngredientsArray);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" />
+          <label htmlFor="filter">Filter by Title</label>
+          <input
+            id="filter"
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
