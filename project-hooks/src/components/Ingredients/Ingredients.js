614674a4d7b9af1270d8d5d4d25d2e96abcb2307
강명구 <mg5566@hanmail.net>
2022-03-19 01:44:16 +0900

    fix: fix infinity loop

--- project-hooks/src/components/Ingredients/Ingredients.js
+++ project-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
@@ -7,21 +7,21 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   // load data when this component is evaluated for the first time
-  useEffect(() => {
-    fetch("https://react-26863-default-rtdb.firebaseio.com/ingredient.json")
-      .then((response) => response.json())
-      .then((responseData) => {
-        const newIngredientsArray = [];
-        for (const key in responseData) {
-          newIngredientsArray.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngredients(newIngredientsArray);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://react-26863-default-rtdb.firebaseio.com/ingredient.json")
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       const newIngredientsArray = [];
+  //       for (const key in responseData) {
+  //         newIngredientsArray.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount,
+  //         });
+  //       }
+  //       setUserIngredients(newIngredientsArray);
+  //     });
+  // }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch("https://react-26863-default-rtdb.firebaseio.com/ingredient.json", {
@@ -48,9 +48,9 @@ const Ingredients = () => {
     );
   };
 
-  const filteredIngredientsHandler = (filteredIngredients) => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
-  };
+  }, []);
 
   return (
     <div className="App">
