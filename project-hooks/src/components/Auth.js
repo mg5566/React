0eeea568dc721782d43aef4b1828380a051e9280
강명구 <mg5566@hanmail.net>
2022-03-18 21:00:00 +0900

    feat: init setting

---
+++ project-hooks/src/components/Auth.js
@@ -0,0 +1,13 @@
+const Auth = (props) => {
+  const loginHandler = () => {};
+
+  return (
+    <div>
+      <Card>
+        <h2>You are not authenticated!</h2>
+        <p>Please log in to continue.</p>
+        <button onClick={loginHandler}>Log In</button>
+      </Card>
+    </div>
+  );
+};
