bb4bd21b6b34bcad9e98c57f467c266d6064f51e
강명구 <mg5566@hanmail.net>
2022-03-19 13:34:53 +0900

    feat: enhance search performance

--- project-hooks/src/components/Ingredients/Search.js
+++ project-hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../UI/Card";
 
 import "./Search.css";
@@ -6,29 +6,35 @@ import "./Search.css";
 const Search = (props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    console.log("Search render", enteredFilter);
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      "https://react-26863-default-rtdb.firebaseio.com/ingredient.json" + query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const newIngredientsArray = [];
-        for (const key in responseData) {
-          newIngredientsArray.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    // console.log("Search render", enteredFilter);
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-26863-default-rtdb.firebaseio.com/ingredient.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const newIngredientsArray = [];
+            for (const key in responseData) {
+              newIngredientsArray.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            // set data
+            onLoadIngredients(newIngredientsArray);
           });
-        }
-        // set data
-        onLoadIngredients(newIngredientsArray);
-      });
+      }
+    }, 500);
   }, [enteredFilter, onLoadIngredients]);
 
   return (
@@ -37,6 +43,7 @@ const Search = (props) => {
         <div className="search-input">
           <label htmlFor="filter">Filter by Title</label>
           <input
+            ref={inputRef}
             id="filter"
             type="text"
             value={enteredFilter}
