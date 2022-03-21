620a54581fbf3787e933cfa890700266bdd4dfd0
강명구 <mg5566@hanmail.net>
2022-03-21 17:48:23 +0900

    feat: create authentication context

--- project-hooks/src/App.js
+++ project-hooks/src/App.js
@@ -1,9 +1,18 @@
-import Ingredients from "./components/Ingredients/Ingredients"
+import { useContext } from "react";
+import { AuthContext } from "./context/auth-context";
+
+import Ingredients from "./components/Ingredients/Ingredients";
+import Auth from "./components/Auth";
 
 function App() {
-  return (
-    <Ingredients />
-  );
+  const auchCtx = useContext(AuthContext);
+
+  let content = <Auth />;
+  if (auchCtx.isAuth) {
+    content = <Ingredients />;
+  }
+
+  return content;
 }
 
 export default App;
